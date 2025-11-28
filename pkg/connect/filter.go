e83957d9b3400a42332ad535b3ffc98de95a85c5
aide <1058165620@qq.com>
2025-11-28 13:43:49 +0800

    fix(connect): optimize node filter logic and handle empty filters
    Improve the SelectNodeFilterOr and SelectNodeFilterAnd functions to handle cases where no filters are provided by returning all nodes. Additionally, optimize the OR filter to break early when a matching filter is found, improving performance.

--- pkg/connect/filter.go
+++ pkg/connect/filter.go
@@ -10,11 +10,16 @@ type NodeFilter func(node selector.Node) bool
 
 func SelectNodeFilterOr(filters ...NodeFilter) selector.NodeFilter {
 	return func(ctx context.Context, nodes []selector.Node) []selector.Node {
+		if len(filters) == 0 {
+			return nodes
+		}
 		newNodes := make([]selector.Node, 0, len(nodes))
 		for _, node := range nodes {
 			anyPass := false
 			for _, filter := range filters {
-				anyPass = anyPass || filter(node)
+				if anyPass = anyPass || filter(node); anyPass {
+					break
+				}
 			}
 			if anyPass {
 				newNodes = append(newNodes, node)
@@ -26,6 +31,9 @@ func SelectNodeFilterOr(filters ...NodeFilter) selector.NodeFilter {
 
 func SelectNodeFilterAnd(filters ...NodeFilter) selector.NodeFilter {
 	return func(ctx context.Context, nodes []selector.Node) []selector.Node {
+		if len(filters) == 0 {
+			return nodes
+		}
 		newNodes := make([]selector.Node, 0, len(nodes))
 		for _, node := range nodes {
 			allPass := true
